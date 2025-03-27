a7ae45e0367344a31ed23c0f1776377bac06fa11
XSajmonX <szymon.beczkowski@gmail.com>
2025-03-27 19:43:09 +0100

    initial commit

---
+++ AI_Methods.py
@@ -0,0 +1,24 @@
+
+
+class Knn:
+
+    def __init__(self,k):
+        self.k = k
+
+    def predict(self):
+        pass
+
+    def euclidean_dist(self):
+        pass
+
+    def manhattan_dist(self):
+        pass
+
+    def minkowski_dist(self):
+        pass
+
+    def squared_euclidean_dist(self):
+        pass
+
+    def chebyshev_dist(self):
+        pass
---
+++ main.py
@@ -0,0 +1,4 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.datasets import make_classification
+from AI_Methods import *
