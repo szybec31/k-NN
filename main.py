acbd33792d7171f6239f08491a062384c6d2cfb8
XSajmonX <szymon.beczkowski@gmail.com>
2025-05-31 18:04:01 +0200

    iteracja i zapis

--- main.py
+++ main.py
@@ -1,57 +1,70 @@
 import numpy as np
 from Classification import Knn_with_OSR
-from Syntetic_data import X_train, X_test, y_train, y_test, flag
+from Syntetic_data import X_known, y_known, X_unknown, y_unknown, flag
 from sklearn.metrics import confusion_matrix
 import pandas as pd
 from sklearn.metrics import balanced_accuracy_score
 from Operations import *
+from datetime import datetime
+
 
 # Lista metryk
 metrics = ["euclidean", "manhattan", "minkowski", "squared_euclidean", "chebyshev"]
-
-unique_classes = np.unique(np.concatenate((y_train, y_test)))
-
-class_labels = list(unique_classes)
-label_names = [str(cls) for cls in class_labels]
-label_names[-1] = f"obca {class_labels[-1]}"
-
+results_summary = []
 
 print("Wyniki predykcji i metryk dla różnych odległości:\n")
 
 for metric in metrics:
-    print("=" * 60)
-    # Inicjalizacja klasyfikatora z daną metryką
-    knn = Knn_with_OSR(k=3, metric=metric)
+    scores = []
+    for i in range(10):
+        print("*************************** Iteracja: {} ***************************".format(i))
+        X_train, X_test, y_train, y_test = train_test_sets(X_known, y_known, X_unknown, y_unknown)
 
-    # Trenowanie modelu
-    knn.fit(X_train, y_train)
+        unique_classes = np.unique(np.concatenate((y_train, y_test)))
 
-    # Predykcja
-    predictions = knn.predict(X_test)
+        class_labels = list(unique_classes)
+        label_names = [str(cls) for cls in class_labels]
+        label_names[-1] = f"obca {class_labels[-1]}"
 
-    # Macierz pomyłek jako DataFrame
-    cm = confusion_matrix(y_test, predictions, labels=class_labels)
-    cm_df = pd.DataFrame(
-        cm,
-        index=[f"Klasa {name}" for name in label_names],
-        columns=[f"Klasa {name}" for name in label_names]
-    )
-    print("Metryka: {}".format(metric))
-    print("Macierz pomyłek:")
-    print("rzeczywiste/przewidziane")
-    print(cm_df)
+        # Inicjalizacja klasyfikatora z daną metryką
+        knn = Knn_with_OSR(k=3, threshold=None, metric=metric)
 
-    # Dokładność
-    balanced_accuracy = balanced_accuracy_score(y_test,predictions)
-    print(f"Procent poprawnych predykcji: {balanced_accuracy:.3f}\n")
+        # Trenowanie modelu
+        knn.fit(X_train, y_train)
 
-    # Utworzenie wykresu dla wybranej metryki
-    ''' 
-   if metric == "euclidean" and flag == 0:
-        y_pred = predictions
-        X_test1 = X_test
-        charts_true_predicted(X_test1,y_test,y_pred,balanced_accuracy,metric)'''
+        # Predykcja
+        predictions = knn.predict(X_test)
 
-    # Utworzenie wykresu dla każdej metryki
-    if flag == 0:
-        charts_true_predicted(X_test,y_test,predictions,balanced_accuracy,metric)
+        # Macierz pomyłek jako DataFrame
+        cm = confusion_matrix(y_test, predictions, labels=class_labels)
+        cm_df = pd.DataFrame(
+            cm,
+            index=[f"Klasa {name}" for name in label_names],
+            columns=[f"Klasa {name}" for name in label_names]
+        )
+        print("Metryka: {}".format(metric))
+        print("Macierz pomyłek:")
+        print("rzeczywiste/przewidziane")
+        print(cm_df)
+
+        # Dokładność
+        balanced_accuracy = balanced_accuracy_score(y_test,predictions)
+        scores.append(balanced_accuracy)
+        print(f"Procent poprawnych predykcji: {balanced_accuracy:.3f}\n")
+
+        # Utworzenie wykresu dla każdej metryki tylko w iteracji 0
+        if flag == 0 and i == 0:
+            charts_true_predicted(X_test,y_test,predictions,balanced_accuracy,metric)
+
+    # Wyznaczenie średniej i odchylenia standardowego dla 10 powtórzeń danej metryki
+    mean_score = round(np.mean(scores),3)
+    std_score = round(np.std(scores),3)
+    results_summary.append([metric, round(mean_score, 4), round(std_score, 4)])
+    print("\n\033[34mŚrednia balanced accuracy: {:.3f} \033[39m".format(mean_score))
+    print("\033[31mOdchylenie standardowe: {:.3f} \033[39m".format(std_score))
+
+df_results = pd.DataFrame(results_summary, columns=["Metryka", "Średnia", "Odchylenie"])
+timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+filename = f"results/wyniki_knn_{timestamp}.csv"
+df_results.to_csv(filename, index=False)
+print(df_results)
