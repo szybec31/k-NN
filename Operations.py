62d3369b891ccb531d029c78315e97f5e8a17012
XSajmonX <szymon.beczkowski@gmail.com>
2025-06-01 16:40:27 +0200

    nowe zbiory danych

--- Operations.py
+++ Operations.py
@@ -27,7 +27,8 @@ def train_test_sets(X_known,y_known,X_unknown,y_unknown):
     y_test_final = np.hstack([y_test_known, y_unknown])
 
     # sprawdzenie kształtu tablic
-    #print("X_test_known shape {}, X_unknown shape {} ".format(X_test_known.shape,X_unknown.shape))
+    print("X_train_known shape {}".format(X_train_known.shape))
+    print("X_test_known shape {}, X_unknown shape {} ".format(X_test_known.shape,X_unknown.shape))
     #print("y_test_known shape {}, y_unknown shape {} ".format(y_test_known.shape, y_unknown.shape))
     #print("Nowa etykieta klasy nieznanej:", unknown_label)
     # Zwraca potrzebne zbiory danych
@@ -67,3 +68,57 @@ def charts_true_predicted(X_test,y_test,y_pred,balanced_accuracy,metric):
     plt.close(fig)
 
     #plt.show()
+
+def zapis_wyników(metric,knn,cm_df,balanced_accuracy):
+    #  Zapis do pliku w tej samej postaci co print
+    """
+    with open("results/results.txt", "a", encoding="utf-8") as f:
+        f.write(f"Threshold: {knn.threshold:.3f}\n")
+        f.write(f"Metryka: {metric}\n")
+        f.write("Macierz pomyłek:\n")
+        f.write("Rzeczywiste / Przewidziane\n")
+        f.write(cm_df.to_string())
+        f.write(f"\nProcent poprawnych predykcji: {balanced_accuracy:.3f}\n\n")
+        f.write("="*60 + "\n")
+    """
+
+    #  Zapis do pliku w postaci kodu w LaTeX
+    with open("results/results.txt", "a", encoding="utf-8") as f:
+        f.write(f"Threshold: {knn.threshold:.3f}\n")
+        f.write(f"Metryka: {metric}\n")
+        f.write("Macierz pomyłek w formie kodu w LaTeX:\n\n")
+
+        n_rows = len(cm_df)
+        n_cols = len(cm_df.columns)
+
+        f.write("\\begin{table}[H]\n")
+        f.write("\\centering\n")
+        f.write(f"\\caption{{Macierz pomyłek metryki {metric}}}\n")
+        f.write("\\vspace{10pt}\n")
+        f.write(f"\\label{{tab:Macierz pomyłek {metric}}}\n")
+        f.write("\\renewcommand{\\arraystretch}{1.3}\n")
+
+        col_format = "|>{\\centering\\arraybackslash}m{0.9cm}"
+        col_format += "|" + "|".join([">{\\centering\\arraybackslash}m{2.7cm}"] * n_cols) + "|"
+        f.write(f"\\begin{{tabular}}{{{col_format}}}\n")
+        f.write("\\hline\n")
+
+        f.write("\\multicolumn{1}{|c|}{} & \\multicolumn{" + str(n_cols) + "}{c|}{\\textbf{Przewidziane}} \\\\\n")
+        f.write("\\cline{2-" + str(n_cols + 1) + "}\n")
+
+        header_row = " & " + " & ".join(f"\\textbf{{{col}}}" for col in cm_df.columns) + " \\\\\n"
+        f.write("\\multirow{" + str(n_rows) + "}{*}{\\rotatebox{90}{\\textbf{Rzeczywiste}}} " + header_row)
+        f.write("\\cline{2-" + str(n_cols + 1) + "}\n")
+
+        for i, (idx, row) in enumerate(cm_df.iterrows()):
+            f.write("& ")
+            row_data = " & ".join(str(val) for val in row)
+            f.write(row_data + " \\\\\n")
+            f.write("\\cline{2-" + str(n_cols + 1) + "}\n")
+
+        f.write("\\hline\n")
+        f.write("\\end{tabular}\n")
+        f.write("\\end{table}\n\n")
+
+        f.write(f"\nProcent poprawnych predykcji: {balanced_accuracy:.3f}\n\n")
+        f.write("=" * 60 + "\n")
